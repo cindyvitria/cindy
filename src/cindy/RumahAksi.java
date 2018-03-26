6e224d144f6c47a328fe49a252db294261cf7b66
Cindyvitria <cindyvitria99@gmail.com>
2018-03-26 20:50:41 +0800

    Mengisi Nilai Atribut

--- src/cindy/RumahAksi.java
+++ src/cindy/RumahAksi.java
@@ -14,5 +14,24 @@ public class RumahAksi {
         Rumah r1 = new Rumah();
         Rumah r2 = new Rumah();
         Rumah r3 = new Rumah();
+        
+        
+        r1.warnaTembok="Biru";
+        r1.warnaAtap="Coklat";
+        r1.tipeRumah=45;
+        r1.bntkJendela="Bulat";
+        r1.jnsPintu="Pintu Keluar";
+        
+        r2.warnaTembok="Pink";
+        r2.warnaAtap="Merah";
+        r2.tipeRumah=100;
+        r2.bntkJendela="KOtak";
+        r2.jnsPintu="Pintu Geser";
+        
+        r3.warnaTembok="Hijau";
+        r3.warnaAtap="Hitam";
+        r3.tipeRumah=54;
+        r3.bntkJendela="Abstrak";
+        r3.jnsPintu="Pintu Dorong";
     }
 }
